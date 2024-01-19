c562df6bb7f80f6bfdac3750f644cae0434ae24d
Danulo <danilyanishevski2001@gmail.com>
2024-01-19 14:28:02 +0200

    finish

--- src/js/search-img.js
+++ src/js/search-img.js
@@ -1,10 +1,7 @@
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
-// Описаний у документації
+
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchForm = document.querySelector('.js-search-form');
@@ -20,24 +17,23 @@ function handleSearch(event) {
 
   const form = event.currentTarget;
   const query = form.elements.query.value;
-
   setTimeout(() => {
     searchPhoto(query)
-      .then(markupPhoto)
       .then(data => {
-        if (data.hits.length <= 0) {
+        if (!data.hits.length) {
           iziToast.error({
             title: 'Error',
             message:
               'Sorry, there are no images matching your search query. Please try again!',
           });
         }
+        ulEl.innerHTML = ('beforeend', markupPhoto(data.hits));
+        modalLightboxGallery.refresh();
       })
       .catch(onFetchError)
       .finally(() => form.reset());
   }, 1000);
 }
-
 function searchPhoto(value) {
   const BAZE_URL = 'https://pixabay.com/api';
   const API_KEY = '41849458-2d98265cf06659a45ba73a30c';
@@ -50,7 +46,6 @@ function searchPhoto(value) {
     return resp.json();
   });
 }
-
 function onFetchError(error) {
   console.error(error);
 
@@ -61,34 +56,38 @@ function onFetchError(error) {
   });
 }
 
-function markupPhoto({ hits }) {
-  const markup = hits
+function markupPhoto(arr) {
+  return arr
     .map(
-      hits => `<li class="gallery-item">
-  
-  <a class="gallery-link" href="${hits.largeImageURL}">
-    <img
-      class="gallery-image"
-      src="${hits.webformatURL}"
-      data-source="${hits.imoriginal}"
-      alt="${hits.tags}"
-    />
-  </a>
-  <ul class="img-list">
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+              width="360"
+            />
+          </a>
+          <ul class="img-list">
   <li class="img-info">
-   <p class="img-text">Likes: <br><span>${hits.likes}</span></p>
-   <p class="img-text">Views: <br><span>${hits.views}</span></p>
-   <p class="img-text">Comment: <br><span>${hits.comments}</span></p>
-   <p class="img-text">Downloads: <br><span>${hits.downloads}</span></p>
+   <p class="img-text">Likes: <br><span>${likes}</span></p>
+   <p class="img-text">Views: <br><span>${views}</span></p>
+   <p class="img-text">Comment: <br><span>${comments}</span></p>
+   <p class="img-text">Downloads: <br><span>${downloads}</span></p>
    </li>
    </ul>
-
-</li>`
+        </li>`
     )
     .join('');
-
-  ulEl.innerHTML = markup;
-  modalLightboxGallery.refresh();
 }
 
 const modalLightboxGallery = new SimpleLightbox('.gallery a', {
